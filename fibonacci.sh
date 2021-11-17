219664b309dd731c17f70421aee6173dbe4fd98a
radhika214 <70576992+radhika214@users.noreply.github.com>
2021-11-17 20:43:33 +0530

    Add files via upload

---
+++ fibonacci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+echo "Program to Find Fibonacci Series"
+  echo "How many number of terms to be generated ?"
+  read n
+  x=0
+  y=1
+  i=2
+  echo "Fibonacci Series up to $n terms is :"
+  echo "$x"
+  echo "$y"
+  while [ $i -lt $n ]
+  do
+      i=`expr $i + 1 `
+      z=`expr $x + $y `
+      echo "$z"
+      x=$y
+      y=$z
+  done
